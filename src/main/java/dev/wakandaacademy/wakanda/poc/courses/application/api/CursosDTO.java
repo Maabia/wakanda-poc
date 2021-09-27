d706d0f992f2bc138cd1826c1f08700f7eaac607
Maabia <maabia.ferry16@gmail.com>
2021-09-27 10:04:22 -0300

    UUI ID adicionado em cursosDTO

--- src/main/java/dev/wakandaacademy/wakanda/poc/courses/application/api/CursosDTO.java
+++ src/main/java/dev/wakandaacademy/wakanda/poc/courses/application/api/CursosDTO.java
@@ -1,12 +1,14 @@
 package dev.wakandaacademy.wakanda.poc.courses.application.api;
 
+import java.util.UUID;
+
 import lombok.Getter;
 
 @Getter
 public class CursosDTO {
 	
+	private UUID id;
 	private String titulo;
-	
 	private String url;
 	
 	public CursosDTO(Cursos cursos) {
