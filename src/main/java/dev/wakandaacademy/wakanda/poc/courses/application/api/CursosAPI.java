55729c4fd0d962ef842beb89ca6618118dd41bd0
Maabia <maabia.ferry16@gmail.com>
2021-09-27 10:03:15 -0300

    endpoint alterado para GET

--- src/main/java/dev/wakandaacademy/wakanda/poc/courses/application/api/CursosAPI.java
+++ src/main/java/dev/wakandaacademy/wakanda/poc/courses/application/api/CursosAPI.java
@@ -5,8 +5,8 @@ package dev.wakandaacademy.wakanda.poc.courses.application.api;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,8 +16,8 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RestController
 public interface CursosAPI {
 	
-	@PostMapping("/v1/cursos")
-	@ResponseStatus(value = HttpStatus.CREATED)
+	@GetMapping("/v1/cursos") // VERIFICAR COM VINI SE VAI USAR O PATH DA TRIBO
+	@ResponseStatus(value = HttpStatus.OK)
 	public ResponseEntity<CursosDTO> criaCurso(@RequestBody @Validated CursosForm cursosForm, UriComponentsBuilder uriBuilder);
 	
 }
