c19492c66572f31e4e1312fac08ab01cbc0417fb
Maabia <maabia.ferry16@gmail.com>
2021-09-30 14:16:42 -0300

    alteração não realizada

--- src/main/java/dev/wakandaacademy/wakanda/poc/courses/application/repository/VideoRepository.java
+++ src/main/java/dev/wakandaacademy/wakanda/poc/courses/application/repository/VideoRepository.java
@@ -2,8 +2,11 @@ package dev.wakandaacademy.wakanda.poc.courses.application.repository;
 
 import java.util.Optional;
 
+import org.springframework.stereotype.Repository;
+
 import dev.wakandaacademy.wakanda.poc.courses.domain.Video;
 
+@Repository
 public interface VideoRepository {
 	Optional<Video> buscaVideoPorId(String idVideo);
 }
