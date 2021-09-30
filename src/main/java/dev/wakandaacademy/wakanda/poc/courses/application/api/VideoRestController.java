a4228f5e8db783f37f96fc5bdaad4587006f8a80
Maabia <maabia.ferry16@gmail.com>
2021-09-30 11:24:32 -0300

    Video controller implementado

--- src/main/java/dev/wakandaacademy/wakanda/poc/courses/application/api/VideoRestController.java
+++ src/main/java/dev/wakandaacademy/wakanda/poc/courses/application/api/VideoRestController.java
@@ -1,5 +1,23 @@
 package dev.wakandaacademy.wakanda.poc.courses.application.api;
 
-public class VideoRestController {
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@RestController
+@AllArgsConstructor
+public class VideoRestController implements VideoAPI {
+	
+	
+	@Override
+	public VideoDTO buscaVideoPorId(String idVideo) {
+		log.info("[Iniciando] Método buscaVideoPorId em Video Controller");
+		Video video = videoService.buscaPorId(idVideo);
+		log.info("[Finalizando] Metodo buscaVideoPorId em Video Controller");
+		return new VideoDTO(video);
+	}
+	
 	
 }
