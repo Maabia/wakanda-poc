8630180514b24413b3af8c67572d7bb16431c734
Maabia <maabia.ferry16@gmail.com>
2021-09-30 13:43:08 -0300

    Adicionei anotação @ Service na classe

--- src/main/java/dev/wakandaacademy/wakanda/poc/courses/application/service/VideoService.java
+++ src/main/java/dev/wakandaacademy/wakanda/poc/courses/application/service/VideoService.java
@@ -1,7 +1,10 @@
 package dev.wakandaacademy.wakanda.poc.courses.application.service;
 
+import org.springframework.stereotype.Service;
+
 import dev.wakandaacademy.wakanda.poc.courses.domain.Video;
 
+@Service
 public interface VideoService {
 	
 	Video buscaVideoPorId(String idVideo);
