5179d851f116814917fb70c290094639cdc21e97
Maabia <maabia.ferry16@gmail.com>
2021-10-01 14:21:22 -0300

    VideoDataJpaService criado e classe ApiException

---
+++ src/main/java/dev/wakandaacademy/wakanda/handler/ApiException.java
@@ -0,0 +1,5 @@
+package dev.wakandaacademy.wakanda.handler;
+
+public class ApiException {
+
+}
---
+++ src/main/java/dev/wakandaacademy/wakanda/poc/video/application/service/VideoDataJpaService.java
@@ -0,0 +1,26 @@
+package dev.wakandaacademy.wakanda.poc.video.application.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import dev.wakandaacademy.wakanda.handler.ApiException;
+import dev.wakandaacademy.wakanda.poc.video.application.repository.VideoRepository;
+import dev.wakandaacademy.wakanda.poc.video.domain.Video;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@AllArgsConstructor
+@Log4j2
+public class VideoDataJpaService implements VideoService {
+	private VideoRepository videoRepository;
+	
+	@Override
+	public Video buscaVideoPorId(String idVideo) {
+		log.info("Iniciando método buscaVideoPorId em VideoDataJpaService");
+		Video videoPorId = videoRepository.buscaVideoPorId(idVideo)
+				.orElseThrow(() ->  ApiException.throwApiException(HttpStatus.NOT_FOUND, "Video não encontrado!"));
+		return null;
+	}
+	
+}
