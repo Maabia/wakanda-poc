eecb9f96ca25d7b11e981695382c162be66d62e3
Maabia <maabia.ferry16@gmail.com>
2021-10-01 16:44:58 -0300

    corrigido erro no videoSpringDataJpaRepository e adicionado log em videoJpaRepository

--- src/main/java/dev/wakandaacademy/wakanda/poc/video/infrastructure/VideoJpaRepository.java
+++ src/main/java/dev/wakandaacademy/wakanda/poc/video/infrastructure/VideoJpaRepository.java
@@ -1,22 +1,26 @@
 package dev.wakandaacademy.wakanda.poc.video.infrastructure;
 
 import java.util.Optional;
-import java.util.UUID;
 
 import org.springframework.stereotype.Component;
 
 import dev.wakandaacademy.wakanda.poc.video.application.repository.VideoRepository;
 import dev.wakandaacademy.wakanda.poc.video.domain.Video;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @Component
 @AllArgsConstructor
+@Log4j2
 public class VideoJpaRepository implements VideoRepository {
-	private final VideoSpringDataJpaRepository videoSpringDataJpaRepository;
+	private VideoSpringDataJpaRepository videoSpringDataJpaRepository;
 	
 	@Override
 	public Optional<Video> buscaVideoPorId(String idVideo) {
-		return videoSpringDataJpaRepository.findById(UUID.fromString(idVideo));
+		log.info("[Inicia] VideoJpaRepository - buscaVideoPorId");
+		Optional<Video> buscaVideoPorId = videoSpringDataJpaRepository.findById(idVideo);
+		log.info("[Finaliza] VideoVideoJpaRepository - buscaVideoPorId");
+		return buscaVideoPorId;
 		
 	}
 
--- src/main/java/dev/wakandaacademy/wakanda/poc/video/infrastructure/VideoSpringDataJpaRepository.java
+++ src/main/java/dev/wakandaacademy/wakanda/poc/video/infrastructure/VideoSpringDataJpaRepository.java
@@ -1,6 +1,6 @@
 package dev.wakandaacademy.wakanda.poc.video.infrastructure;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +10,5 @@ import dev.wakandaacademy.wakanda.poc.video.domain.Video;
 
 @Repository
 public interface VideoSpringDataJpaRepository extends JpaRepository<Video, UUID> {
-	List<Video> findByVideo(String idVideo);
+	Optional<Video> findById(String idVideo);
 }
