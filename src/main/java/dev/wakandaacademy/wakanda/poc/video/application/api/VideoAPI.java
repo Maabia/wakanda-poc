feccc783972bfd932c642d25df94d3cb2103e354
Maabia <maabia.ferry16@gmail.com>
2021-10-01 13:49:46 -0300

    adicionado RequestMapping

--- src/main/java/dev/wakandaacademy/wakanda/poc/video/application/api/VideoAPI.java
+++ src/main/java/dev/wakandaacademy/wakanda/poc/video/application/api/VideoAPI.java
@@ -4,14 +4,15 @@ package dev.wakandaacademy.wakanda.poc.video.application.api;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-// SERIA NECESSÁRIO POR REQUESTMAPPING AQUI?
+@RequestMapping("/v1/video")
 public interface VideoAPI {
 	
-	@GetMapping("/v1/video/{idVideo}") 
+	@GetMapping("/{idVideo}") 
 	@ResponseStatus(value = HttpStatus.OK)
 	VideoDTO buscaVideoPorId(@PathVariable String idVideo); 
 	
