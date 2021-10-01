4c83d29bd0fbcba85d7aeac5fa7ec30b5d221895
Maabia <maabia.ferry16@gmail.com>
2021-10-01 14:34:23 -0300

    ErrorApiDTO finalizado

--- src/main/java/dev/wakandaacademy/wakanda/handler/ErrorApiDTO.java
+++ src/main/java/dev/wakandaacademy/wakanda/handler/ErrorApiDTO.java
@@ -7,8 +7,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ErrorApiDTO {
 	private String message;
