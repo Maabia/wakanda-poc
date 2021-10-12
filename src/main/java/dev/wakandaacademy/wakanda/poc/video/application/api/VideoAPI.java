87592fac44775428eaccca121972dfa28a9e8a67
Maabia <maabia.ferry16@gmail.com>
2021-10-12 12:31:06 -0300

    codigo voltou para o estado original após as modificações

--- src/main/java/dev/wakandaacademy/wakanda/poc/video/application/api/VideoAPI.java
+++ src/main/java/dev/wakandaacademy/wakanda/poc/video/application/api/VideoAPI.java
@@ -2,7 +2,9 @@ package dev.wakandaacademy.wakanda.poc.video.application.api;
 
 
 
+
 import org.springframework.http.HttpStatus;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,11 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/video")
 public interface VideoAPI {
 	
+	
+
+
 	
 	@GetMapping("/{idVideo}") 
 	@ResponseStatus(value = HttpStatus.OK)
--- src/main/java/dev/wakandaacademy/wakanda/poc/video/application/repository/VideoRepository.java
+++ src/main/java/dev/wakandaacademy/wakanda/poc/video/application/repository/VideoRepository.java
@@ -2,11 +2,13 @@ package dev.wakandaacademy.wakanda.poc.video.application.repository;
 
 import java.util.Optional;
 
+
 import org.springframework.stereotype.Repository;
 
 import dev.wakandaacademy.wakanda.poc.video.domain.Video;
 
 @Repository
-public interface VideoRepository {
+public interface VideoRepository 
+ {
 	Optional<Video> buscaVideoPorId(Long idVideo);
 }
