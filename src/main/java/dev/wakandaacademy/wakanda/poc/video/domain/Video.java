0e3c060734195e57154540df8acc79b6ab4e2639
Maabia <maabia.ferry16@gmail.com>
2021-10-11 22:38:40 -0300

    @builder adicionado

--- src/main/java/dev/wakandaacademy/wakanda/poc/video/domain/Video.java
+++ src/main/java/dev/wakandaacademy/wakanda/poc/video/domain/Video.java
@@ -6,13 +6,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-//@Builder
+@Builder
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 public class Video {
 	
